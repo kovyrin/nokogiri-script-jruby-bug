f7ef43399722b958f1c81ad4c52db3737a22d4de
Oleksiy Kovyrin <alexey@kovyrin.net>
2017-10-12 13:25:09 -0400

    Update test.rb

--- test.rb
+++ test.rb
@@ -4,9 +4,16 @@ require 'bundler'
 Bundler.require
 require 'nokogiri'
 
+# Parse a simple html doc
 html = '<html><head><script>document.alert("hello from js");</script><body>hello</body>'
 doc = Nokogiri::HTML(html)
+
+# Get the script node
 script_node = doc.xpath('//script').first
+
+# Get script's content
 script_content = script_node.children.first
 puts "Script content: #{script_content.inspect}"
+
+# Test it
 raise "Whoa! Script content is text instead of CDATA" if script_content.text?
